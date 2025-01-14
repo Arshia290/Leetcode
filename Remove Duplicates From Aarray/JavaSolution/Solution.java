76eaa7c68dc4b1a3cf9e2cc7ede925ec8932a522
Arshia Rashedzadeh <arshiarashedzadeh@gmail.com>
2025-01-14 11:47:35 -0500

    Add files via upload

---
+++ Remove Duplicates From Aarray/JavaSolution/Solution.java
@@ -0,0 +1,16 @@
+package removeDuplicatesFromArray;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static int removeDuplicates(int[] nums) {
+        int index = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i - 1]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
